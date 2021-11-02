d42a028735be61b9cb0a53956b902280680bc6dc
Elves Brito <elvesbd@yahoo.com>
2021-11-02 10:48:22 -0300

    updated

--- src/app/controllers/ContactController.js
+++ src/app/controllers/ContactController.js
@@ -9,21 +9,23 @@ class ContactController {
 
   async show(request, response) {
     const { id } = request.params;
-
     const contact = await ContactsRepository.findById(id);
+
     if (!contact) {
-      return response.status(404).json({ error: 'User not found' });
+      return response.status(404).json({ error: 'Contact not found' });
     }
     response.json(contact);
   }
 
   async store(request, response) {
     const { name, email, phone, category_id } = request.body;
+
     if (!name) {
       return response.status(400).json({ error: 'Name is required' });
     }
 
     const contactExists = await ContactsRepository.findByEmail(email);
+
     if (contactExists) {
       return response.status(400).json({ error: 'This e-mail is already been taken' });
     }
@@ -50,6 +52,7 @@ class ContactController {
     }
 
     const contactByEmail = await ContactsRepository.findByEmail(email);
+
     if (contactByEmail && contactByEmail.id !== id) {
       return response.status(400).json({ error: 'This e-mail already in use' });
     }
